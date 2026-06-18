7f61df535cd108755bcdd3ebd6f18b85f27ef01d
Anthony-svg <anthony.melendez07.am@gmail.com>
2026-06-18 17:03:13 -0500

    get values in form inventario

--- src/app/paginas/inventario/inventario.ts
+++ src/app/paginas/inventario/inventario.ts
@@ -1,29 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-inventario',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './inventario.html',
   styleUrl: './inventario.css',
 })
 export class Inventario {
-
+  private readonly formBuilder = inject(FormBuilder);
   httpClient=inject(HttpClient)
 
-  InventarioData={
+  form = this.formBuilder.group({
     productoId:"",
     productoNombre:"",
     stockDisponible:0,
     reponerStock:0
   }
+  )
 
-  onSubmit(){
-    let apiInventory=environment.apiURL+'/api/Inventario'
-    this.httpClient.post(apiInventory,this.InventarioData).subscribe((result)=>{
-      
-    })
-    alert('form submitted'+JSON.stringify(this.InventarioData))
+  guardarInventario(){
+   console.log(this.form.value)
   }
+
 }
